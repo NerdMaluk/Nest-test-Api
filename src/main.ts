10a95291210e6ba886b6b3f5f11ae614bca691d4
NerdMaluk <flaviojesussoares@gmail.com>
2025-11-17 16:10:36 -0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // permite seu frontend
+    credentials: true, // se precisar enviar cookies
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3333);
 }
